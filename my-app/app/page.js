efe20dc34342e9c3d5da3342eb6d1fdae0538fd6
Calebe Martins <calebe.martins@academico.ifpb.edu.br>
2025-11-19 11:57:45 -0300

    primeiro commit

---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div class name="container">
+      <h1>
+        Bem-vindo à biblioteca do IFPB!
+      </h1>
+    </div>
+  );
+}
