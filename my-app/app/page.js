57c963a139a9cefc54603619e1b8e6b5db5a2f31
Calebe Martins <calebe.martins@academico.ifpb.edu.br>
2025-11-25 10:41:15 -0300

    feat: adicionar RootLayout com estilos globais e metadados

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,10 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div class name="container">
+    <div className="container">
       <h1>
         Bem-vindo à biblioteca do IFPB!
       </h1>
+      <Image
+        src="/ifpb-logo.png"
+        alt="IFPB Logo"
+        width={200}
+        height={300}
+      />
+      <p>
+        Explore nossa coleção de livros, artigos e recursos digitais.
+      </p>
     </div>
   );
 }
