f8456a7a6e242aaca335c87d0407b7e0a3bf9deb
Calebe Martins <calebe.martins@academico.ifpb.edu.br>
2025-11-26 12:05:48 -0300

    feat: adicionar componente Header com links de navegação

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
+        <Header />
         <main style={{ minHeight: "calc(100vh - 100px)", paddingBottom: "70px" }}>
         {children}
         </main>
